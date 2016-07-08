5383673d83589d1244551ebd73130145e1238ec8
roma.sck <roma.sck@gmail.com>
2016-07-09 00:50:52 +0300

    remove unused constants

--- app/src/main/java/kultprosvet/com/calculator/Const.java
+++ app/src/main/java/kultprosvet/com/calculator/Const.java
@@ -5,15 +5,6 @@ public class Const {
     protected static final int ZERO_VALUE = 0;
     protected static final int ONE_VALUE = 1;
     protected static final String EMPTY = "";
-    protected static final String ONE = "1";
-    protected static final String TWO = "2";
-    protected static final String THREE = "3";
-    protected static final String FOUR = "4";
-    protected static final String FIVE = "5";
-    protected static final String SIX = "6";
-    protected static final String SEVEN = "7";
-    protected static final String EIGHT = "8";
-    protected static final String NINE = "9";
     protected static final String ZERO = "0";
     protected static final String COMMA = ".";
     protected static final String ZERO_DOUBLE = "0.0";
@@ -21,9 +12,5 @@ public class Const {
     protected static final String OPER_DELIM = "/";
     protected static final String OPER_PLUS = "+";
     protected static final String OPER_MINUS = "-";
-    protected static final String EQUALS = "=";
-    protected static final String CLEAR = "C";
-    protected static final String DELETE = "<";
-    protected static final String TOGGLE = "+/-";
     protected static final String SPACE = " ";
 }
